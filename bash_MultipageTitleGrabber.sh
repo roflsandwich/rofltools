521a8275a0a046f893e5292263ed8c80b23e8159
roflsandwich <11023955+roflsandwich@users.noreply.github.com>
2023-05-27 18:44:05 +0100

    Update and rename SubdomainTitleGrabber.sh to bash_MultipageTitleGrabber.sh

--- bash_MultipageTitleGrabber.sh
+++ bash_MultipageTitleGrabber.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 if [ -z "$1" ]
         then
-                echo "Usage: ${0} subdomainFile.txt"
+                echo "Usage: ${0} urls_file.txt"
                 exit
         fi
 for URL in $(cat $1)
         do
-                URL="https://"$URL
                 STATUS=$(curl --connect-timeout 5 -k -s $URL | grep -o -i "<title>.*</title>" | sed "s/<[^>]*>/\n/g" | tr -d '\n' )
                 if [ -z "$STATUS" ]
                 then
